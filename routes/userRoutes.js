fa12e4c2e266a566dd10da5b47ca6b4296aa02aa
robinhood0107_Desktop <pjj4678@gmail.com>
2025-12-01 02:57:53 +0900

    임시로 만들어둔 필요없는 url 삭제하고 profile.ejs와 일치하게 url 변경

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,23 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // GET /user/profile - 마이페이지
-router.get('/profile', userController.profile || ((req, res) => {
-    const tab = req.query.tab || 'sales';
-    res.render('user/profile', { 
-        user: req.user || null,
-        products: [],
-        tab: tab
-    });
-}));
-
-// GET /user/edit - 프로필 수정 페이지
-router.get('/edit', userController.getEditPage || ((req, res) => {
-    res.render('user/edit', { user: req.user || null });
-}));
+router.get('/profile', userController.profile);
 
 // POST /user/edit - 프로필 수정 처리
-router.post('/edit', userController.update || ((req, res) => {
-    res.redirect('/user/profile');
-}));
+router.post('/edit', userController.update);
 
 module.exports = router;
